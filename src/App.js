e1f064d4796eace47d61935197b2525d354e6ee0
Fireless <492429553@qq.com>
2019-11-27 17:06:12 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Button>antd</Button>
+    </div>
+  );
+}
+
+export default App;
